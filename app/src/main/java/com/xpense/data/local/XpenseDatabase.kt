7c283ccc7e86fc7a2c35fa0f688e25b53c05df17
Kelvin Kioko DVTMC <kelvinmkioko@gmail.com>
2023-01-27 17:34:33 +0300

    updated the database class with all the entities

--- app/src/main/java/com/xpense/data/local/XpenseDatabase.kt
+++ app/src/main/java/com/xpense/data/local/XpenseDatabase.kt
@@ -2,9 +2,20 @@ package com.xpense.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.xpense.data.local.entity.AccountEntity
+import com.xpense.data.local.entity.CategoryEntity
+import com.xpense.data.local.entity.CurrencyEntity
+import com.xpense.data.local.entity.TransactionEntity
+import com.xpense.data.local.entity.TransactionItemsEntity
 
 @Database(
-    entities = [],
+    entities = [
+        AccountEntity::class,
+        CategoryEntity::class,
+        CurrencyEntity::class,
+        TransactionEntity::class,
+        TransactionItemsEntity::class
+    ],
     version = 1
 )
 abstract class XpenseDatabase : RoomDatabase()
